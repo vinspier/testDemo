2336a0d89baa829a7fb7e4a5fbd07a1f879ef396
vinspier <954579147@qq.com>
2019-01-07 17:44:39 +0800

    通过反射测试String类的value值可变

---
+++ src/TestStringFiledVariable.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Field;
+
+public class TestStringFiledVariable {
+    public static void main(String[] args) throws Exception {
+        String s = new String("abcdefgh");
+        System.out.println(s.length());
+        Class clazz = s.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        for(int i = 0; i < fields.length; i++){
+            System.out.println(fields[i].getName());
+        }
+        System.out.println("----------------------------------");
+        Field field = clazz.getDeclaredField("value");
+        field.setAccessible(true);
+        char[] value = (char[])field.get(s);
+        value[1] = '范';
+        value[value.length-1] = '婕';
+        System.out.println(s + "  length: " + s.length());
+    }
+}
